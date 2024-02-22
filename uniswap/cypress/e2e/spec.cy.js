9ef96a414510dd10dbe5fdef2ebeb44df73b8397
Shexoni <bogdan.gft@gmail.com>
2024-02-22 16:21:22 -0500

    regular tests

--- uniswap/cypress/e2e/spec.cy.js
+++ uniswap/cypress/e2e/spec.cy.js
@@ -1,13 +1,24 @@
-describe('connect wallet spec', () => {
+describe('testing swap functionality', () => {
   beforeEach(() => {
     cy.visit('/');
   });
-
-  it('should connect wallet with success', () => {
+  it.only('successfully swaps ETH for WETH', () => {
     cy.get('[data-testid="navbar-connect-wallet"]').click();
     cy.get('.WalletModal__OptionGrid-sc-176d03d-1.uWluI')
       .contains('MetaMask')
       .click();
     cy.acceptMetamaskAccess();
+    cy.get('.token-symbol-container')
+      .contains('Select token')
+      .click({ force: true });
+    cy.get("div[title='Wrapped Ether']")
+      .contains('Wrapped Ether')
+      .click({ force: true });
+    cy.get('input#swap-currency-input').type('0.5');
+    cy.get('input#swap-currency-output').should('have.value', '0.5');
+    cy.get('[data-testid="wrap-button"]').click();
+    s;
+    cy.confirmMetamaskPermissionToSpend();
   });
+  it('shows inefficient funds', () => {});
 });
