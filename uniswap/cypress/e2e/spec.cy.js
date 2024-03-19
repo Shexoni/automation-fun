ed48292b91cc130c29eb1861ba31a431aa85b570
Shexoni <bogdan.gft@gmail.com>
2024-03-19 05:53:00 -0400

    additional tests

--- uniswap/cypress/e2e/spec.cy.js
+++ uniswap/cypress/e2e/spec.cy.js
@@ -2,7 +2,11 @@ describe('testing swap functionality', () => {
   beforeEach(() => {
     cy.visit('/');
   });
-  it.only('successfully swaps ETH for WETH', () => {
+
+  afterEach(() => {
+    cy.disconnectMetamaskWalletFromAllDapps();
+  });
+  it('successfully swaps ETH for WETH', () => {
     cy.get('[data-testid="navbar-connect-wallet"]').click();
     cy.get('.WalletModal__OptionGrid-sc-176d03d-1.uWluI')
       .contains('MetaMask')
@@ -24,5 +28,40 @@ describe('testing swap functionality', () => {
       expect(str).to.equal('Wrapped');
     });
   });
-  it('shows inefficient funds', () => {});
+  it('shows inefficient funds', () => {
+    cy.get('[data-testid="navbar-connect-wallet"]').click();
+    cy.get('.WalletModal__OptionGrid-sc-176d03d-1.uWluI')
+      .contains('MetaMask')
+      .click();
+    cy.acceptMetamaskAccess();
+    cy.get('.token-symbol-container')
+      .contains('Select token')
+      .click({ force: true });
+    cy.get("div[title='Wrapped Ether']")
+      .contains('Wrapped Ether')
+      .click({ force: true });
+    cy.get('input#swap-currency-input').type('999999');
+    cy.get('input#swap-currency-output').should('have.value', '999999');
+    cy.get('[data-testid="wrap-button"]')
+      .should('be.disabled')
+      .contains('Insufficient ETH balance');
+  });
+
+  it('test the change position of tokens button', () => {});
+  it('test inputting things other than numbers', () => {});
+
+  // it.only('user should see balance amount', () => {
+  //   cy.get('[data-testid="navbar-connect-wallet"]').click();
+  //   cy.get('.WalletModal__OptionGrid-sc-176d03d-1.uWluI')
+  //     .contains('MetaMask')
+  //     .click();
+  //   cy.acceptMetamaskAccess();
+  //   let uniswapBalance;
+
+  //   cy.get('[data-testid="currency-display-component__text"]  ').then((value) => {
+  //     let walletBalance = value.text();
+  //     uniswapBalance = walletBalance.toFixed(2);
+  //   });
+  //   cy.get('[data-testid="balance-text"]').should('have.text', uniswapBalance); imas dva ova selektora na strani
+  // });
 });
